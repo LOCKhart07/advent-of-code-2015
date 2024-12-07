86127b920f77639043a7f97467ea758a721be5ea
Jenslee Dsouza <jenslee.10715879@ltimindtree.com>
2024-12-08 00:17:45 +0530

    day4

---
+++ day4/part1.py
@@ -0,0 +1,21 @@
+"""AoC_Year2015_Day4_Part1"""
+
+INPUT_PATH = r"input.txt"
+EXAMPLE_INPUT_PATH = r"example1.txt"
+
+with open(INPUT_PATH, "r") as f:
+    secret_key = f.read()
+
+import hashlib
+
+i = 0
+while True:
+    i += 1
+    s = secret_key + str(i)
+    md5_hash = hashlib.md5(s.encode("utf-8")).hexdigest()
+    if md5_hash[:5] == "00000":
+        print(i)
+        break
+#     hashes.append(md5_hash)
+
+# print(hashes)
