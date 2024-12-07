d46e59a982f4ff4ffaaef325de7f5e3b093b1a8d
Jenslee Dsouza <jenslee.10715879@ltimindtree.com>
2024-12-07 16:48:04 +0530

    day2

---
+++ day2/part1.py
@@ -0,0 +1,21 @@
+"""AoC_Year2015_Day2_Part1"""
+
+INPUT_PATH = "input.txt"
+EXAMPLE_INPUT_PATH = "example1.txt"
+
+s = 0
+with open(INPUT_PATH, "r") as f:
+    while line := f.readline():
+        sides = [int(i) for i in line.strip().split("x")]
+        smallest_sides = sorted(sides)[:-1]
+        sa = (
+            (2 * sides[0] * sides[1])
+            + (2 * sides[1] * sides[2])
+            + (2 * sides[2] * sides[0])
+            + smallest_sides[0] * smallest_sides[1]
+        )
+        # print(sides, "          ", sa)
+        s += sa
+
+
+print(s)
