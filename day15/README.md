9bee25ce12e4a21a73b826b7406c6b03a45601ec
Jenslee Dsouza <jenslee.10715879@ltimindtree.com>
2024-12-10 00:16:51 +0530

    day15

---
+++ day15/README.md
@@ -0,0 +1,5 @@
+# Science for Hungry People
+
+**Advent of Code: Day 15, 2015**
+
+See https://adventofcode.com/2015/day/15
---
+++ day15/part1.py
@@ -0,0 +1,51 @@
+'''AoC_Year2015_Day15_Part1'''
+
+
+INPUT_PATH = "input.txt"
+EXAMPLE_INPUT_PATH = "example1.txt"
+
+
+import itertools, re
+
+r_str = r"([A-Za-z]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)"
+ingredients = []
+with open(INPUT_PATH, "r") as f:
+
+    # while line := f.readline().strip():
+    #     ingredients.append(re.findall(r_str, line))
+    ingredients = re.findall(r_str, f.read())
+
+# for i in ingredients:
+#     print(i)
+
+
+max_total_scores = 0
+
+
+def get_sum_of_ingredients(teaspoons):
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+    for i, ingredient in enumerate(ingredients):
+        a += int(ingredient[0 + 1]) * teaspoons[i]
+        b += int(ingredient[1 + 1]) * teaspoons[i]
+        c += int(ingredient[2 + 1]) * teaspoons[i]
+        d += int(ingredient[3 + 1]) * teaspoons[i]
+        e += int(ingredient[4 + 1]) * teaspoons[i]
+    a = max(0, a)
+    b = max(0, b)
+    c = max(0, c)
+    d = max(0, d)
+    return a * b * c * d
+ 
+
+
+ma = 0
+for combination in itertools.product(range(101), repeat=len(ingredients)):
+    if sum(combination) == 100:
+        ma = max(ma, get_sum_of_ingredients(combination))
+    continue
+
+print(ma)
